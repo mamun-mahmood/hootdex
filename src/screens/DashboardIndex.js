1bbeb425519c9fac092d692d841229f980e3d688
Aakash Bhardwaj <officialaakashbhardwaj@gmail.com>
2022-07-06 17:19:12 +0530

    latest deployed

--- src/screens/DashboardIndex.js
+++ src/screens/DashboardIndex.js
@@ -52,7 +52,7 @@ const DashboardIndex = ({ user, pecuCoins }) => {
   };
   const [tab, setTab] = React.useState(0);
 
-  let tier = user.tier || 1;
+  let tier = user.tier || 2;
   return (
     <>
       <Divider color="black" />
