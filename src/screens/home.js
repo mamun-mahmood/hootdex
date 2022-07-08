140d13f3193b126bca71bfb4f40b27b34742c8ea
Aakash Bhardwaj <officialaakashbhardwaj@gmail.com>
2022-07-08 21:31:29 +0530

    removed particle js as it was overloading

--- src/screens/home.js
+++ src/screens/home.js
@@ -3,21 +3,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Chart from "./chart";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
 export default function Home() {
-  const particlesInit = async (main) => {
-    console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   const poolTableAttributes = [
     "Id",
     "Name",
@@ -180,82 +167,7 @@ export default function Home() {
   return (
     <>
       <div style={{ position: 'absolute', zIndex: "-1", width: '100%', height: "100%" }}>
-        <Particles
-        style={{ position: 'absolute', zIndex: "-100", width: '100%', height: "100%" }}
-          options={{
-            fpsLimit: 40,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 2,
-                },
-                repulse: {
-                  distance: 100,
-                  duration: 0.8,
-                },
-              },
-            },
-            particles: {
-              zIndex: {
-                value: 10,
-              },
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              collisions: {
-                enable: false,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 2,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 1200,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-        />
+      
       </div>
       <div className="screen" style={{zIndex: "100", position: 'sticky', width: '100%', }}>
         {/* <div className="banner-hero" >   <h1 className="primary__title">Available Pools</h1>
