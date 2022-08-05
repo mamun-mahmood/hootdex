a2d9b3b780045bc6e83f19e76e3942b7e5b62dca
Aakash Bhardwaj <officialaakashbhardwaj@gmail.com>
2022-08-06 00:53:07 +0530

    error fixed

--- src/components/Graphs/PoolGraph.js
+++ src/components/Graphs/PoolGraph.js
@@ -1,7 +1,7 @@
-import { Box, Button, Typography } from "@mui/material";
-import { red } from "@mui/material/colors";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { Box, Button, Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {
   AreaChart,
   Area,
@@ -11,26 +11,26 @@ import {
   Tooltip,
   ResponsiveContainer,
   LineChart,
-  Line,
-} from "recharts";
-import url from "../../serverUrl";
+  Line
+} from 'recharts';
+import url from '../../serverUrl';
 function convertToInternationalCurrencySystem(labelValue) {
   // Nine Zeroes for Billions
   return Math.abs(Number(labelValue)) >= 1.0e9
-    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + "b"
+    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + 'b'
     : // Six Zeroes for Millions
     Math.abs(Number(labelValue)) >= 1.0e6
-    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "m"
+    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + 'm'
     : // Three Zeroes for Thousands
     Math.abs(Number(labelValue)) >= 1.0e3
-    ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "k"
+    ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + 'k'
     : Math.abs(Number(labelValue));
 }
 const PoolGraph = ({ id, pool, currentValue }) => {
   // console.log("Pool Graph Renders");
   const [chartBtn, setChartBtn] = useState(2);
   const [chartData, setChartData] = useState([]);
-  const [filter, setFilter] = useState("current_hourly");
+  const [filter, setFilter] = useState('current_hourly');
   console.log(pool);
   const getChatData = (target) => {
     setFilter(target);
@@ -54,57 +54,55 @@ const PoolGraph = ({ id, pool, currentValue }) => {
       });
   };
   useEffect(() => {
-    getChatData("current_hourly");
+    getChatData('current_hourly');
   }, []);
 
   return (
     <>
       <Box
         sx={{
-          display: { xs: "inline-block", sm: "flex" },
-          justifyContent: "space-between",
-          width: "100%",
-          height: 30,
+          display: { xs: 'inline-block', sm: 'flex' },
+          justifyContent: 'space-between',
+          width: '100%',
+          height: 30
         }}
       >
         <div>
           <p
             style={{
-              marginTop: "0.5rem",
-              fontSize: "24px",
-              color: "white",
-              fontWeight: "bold",
+              marginTop: '0.5rem',
+              fontSize: '24px',
+              color: 'white',
+              fontWeight: 'bold'
             }}
           >
-            {" "}
-            {convertToInternationalCurrencySystem(
-              chartData[0].TVL
-            )}
+            {chartData.length > 0 &&
+              convertToInternationalCurrencySystem(chartData[0].TVL)}
           </p>
           {/* <p className="pool-page-t1">{date}</p> */}
         </div>
         <div
           style={{
-            backgroundColor: "rgb(44, 47, 54)",
-            borderRadius: "12px",
-            textAlign: "center",
+            backgroundColor: 'rgb(44, 47, 54)',
+            borderRadius: '12px',
+            textAlign: 'center'
           }}
           className="dsparound"
         >
           <p
-            className={`${chartBtn === 1 && "chart_btn_selected"} chart_btn`}
+            className={`${chartBtn === 1 && 'chart_btn_selected'} chart_btn`}
             onClick={() => setChartBtn(1)}
           >
             Volume
           </p>
           <p
-            className={`${chartBtn === 2 && "chart_btn_selected"} chart_btn`}
+            className={`${chartBtn === 2 && 'chart_btn_selected'} chart_btn`}
             onClick={() => setChartBtn(2)}
           >
             TVL
           </p>
           <p
-            className={`${chartBtn === 3 && "chart_btn_selected"} chart_btn`}
+            className={`${chartBtn === 3 && 'chart_btn_selected'} chart_btn`}
             onClick={() => setChartBtn(3)}
           >
             Price
@@ -113,69 +111,69 @@ const PoolGraph = ({ id, pool, currentValue }) => {
       </Box>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "1rem",
-          minWidth: "100%",
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '1rem',
+          minWidth: '100%'
         }}
       >
         <Button
           className={`${
-            filter === "current_hourly"
-              ? "header-link chart-button activeButton"
-              : "header-link chart-button"
+            filter === 'current_hourly'
+              ? 'header-link chart-button activeButton'
+              : 'header-link chart-button'
           }`}
           onClick={() => {
-            getChatData("current_hourly");
+            getChatData('current_hourly');
           }}
         >
           1H
         </Button>
         <Button
           className={`${
-            filter === "current_weekly"
-              ? "header-link chart-button activeButton"
-              : "header-link chart-button"
+            filter === 'current_weekly'
+              ? 'header-link chart-button activeButton'
+              : 'header-link chart-button'
           }`}
-          sx={{ display: { xs: "none", md: "block" } }}
+          sx={{ display: { xs: 'none', md: 'block' } }}
           onClick={() => {
-            getChatData("current_weekly");
+            getChatData('current_weekly');
           }}
         >
           1W
         </Button>
         <Button
           className={`${
-            filter === "current_monthly"
-              ? "header-link chart-button activeButton"
-              : "header-link chart-button"
+            filter === 'current_monthly'
+              ? 'header-link chart-button activeButton'
+              : 'header-link chart-button'
           }`}
           onClick={() => {
-            getChatData("current_monthly");
+            getChatData('current_monthly');
           }}
         >
           3M
         </Button>
         <Button
           className={`${
-            filter === "current_quaterly"
-              ? "header-link chart-button activeButton"
-              : "header-link chart-button"
+            filter === 'current_quaterly'
+              ? 'header-link chart-button activeButton'
+              : 'header-link chart-button'
           }`}
           onClick={() => {
-            getChatData("current_quaterly");
+            getChatData('current_quaterly');
           }}
         >
           1m
         </Button>
         <Button
           className={`${
-            filter === "current_yearly"
-              ? "header-link chart-button activeButton"
-              : "header-link chart-button"
+            filter === 'current_yearly'
+              ? 'header-link chart-button activeButton'
+              : 'header-link chart-button'
           }`}
           onClick={() => {
-            getChatData("current_yearly");
+            getChatData('current_yearly');
           }}
         >
           1Y
@@ -183,14 +181,14 @@ const PoolGraph = ({ id, pool, currentValue }) => {
       </div>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          height: "100%",
-          marginTop: "1rem",
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100%',
+          marginTop: '1rem'
         }}
       >
-        <ResponsiveContainer width={"100%"} height={300}>
-          <LineChart height={"100%"} data={chartData}>
+        <ResponsiveContainer width={'100%'} height={300}>
+          <LineChart height={'100%'} data={chartData}>
             <Line
               type="monotone"
               dataKey="TVL"
